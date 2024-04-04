a6eabe994139e8f11e385ceb43b48769ca1ee9f0
Matti Hernesniemi <matti.hernesniemi@exove.fi>
2024-04-04 22:28:25 +0300

    add cloudflare integration

--- frontend-astro/astro.config.mjs
+++ frontend-astro/astro.config.mjs
@@ -1,8 +1,11 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [preact()],
+  output: "hybrid",
+  adapter: cloudflare(),
 });
