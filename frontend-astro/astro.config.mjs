9c43d9ecca51874fae2eb75508dddbeab2cc4f4b
Matti Hernesniemi <matti.hernesniemi@exove.fi>
2024-04-04 22:18:22 +0300

    test clean

--- frontend-astro/astro.config.mjs
+++ frontend-astro/astro.config.mjs
@@ -1,16 +1,4 @@
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
-import pagefind from "astro-pagefind";
-import tailwind from "@astrojs/tailwind";
-
-import cloudflare from "@astrojs/cloudflare";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  output: "hybrid",
-  build: {
-    format: "file",
-  },
-  integrations: [react(), tailwind(), pagefind()],
-  adapter: cloudflare(),
-});
+export default defineConfig({});
