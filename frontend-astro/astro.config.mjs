85a881082383cb56e9d32b83cdbd7b2c52bb01aa
Matti Hernesniemi <matti.hernesniemi@exove.fi>
2024-04-05 17:30:35 +0300

    switch to react

--- frontend-astro/astro.config.mjs
+++ frontend-astro/astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from "astro/config";
-import preact from "@astrojs/preact";
 import cloudflare from "@astrojs/cloudflare";
 import sitemap from "@astrojs/sitemap";
-
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact({ compat: true }), sitemap(), tailwind()],
+  integrations: [sitemap(), tailwind(), react()],
   output: "hybrid",
   adapter: cloudflare(),
-  site: "https://strapi-astro.pages.dev/",
+  site: "https://strapi-astro.pages.dev/"
 });
