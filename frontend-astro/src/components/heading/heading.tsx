04e5ede38edf84fa827a4bcdd3f206d2f418d723
Matti Hernesniemi <matti.hernesniemi@exove.fi>
2024-04-05 16:39:01 +0300

    default heading component

--- frontend-astro/src/components/heading/heading.tsx
+++ frontend-astro/src/components/heading/heading.tsx
@@ -1,3 +1,6 @@
+import React from "preact/compat";
+import clsx from "clsx";
+
 interface HeadingProps {
   level: "h1" | "h2" | "h3" | "h4";
   size?: "xl" | "large" | "medium" | "small";
@@ -13,5 +16,18 @@ export default function Heading({
 }: HeadingProps) {
   const Tag = level;
 
-  return <Tag>{children}</Tag>;
+  return (
+    <Tag
+      className={clsx(
+        "font-bold break-words",
+        size === "xl" && "text-3xl lg:leading-tight lg:text-5xl mb-10",
+        size === "large" && "text-2xl lg:text-3xl mb-6",
+        size === "medium" && "text-xl lg:text-2xl mb-6",
+        size === "small" && "text-xl",
+        zeroMargin && "!mb-0",
+      )}
+    >
+      {children}
+    </Tag>
+  );
 }
