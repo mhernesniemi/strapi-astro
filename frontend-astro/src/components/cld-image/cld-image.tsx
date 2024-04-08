59bc8582d5c94a5c913742cb830f4b8f6a63c313
Matti Hernesniemi <matti.hernesniemi@exove.fi>
2024-04-09 01:45:10 +0300

    add image aspect ratio

--- frontend-astro/src/components/cld-image/cld-image.tsx
+++ frontend-astro/src/components/cld-image/cld-image.tsx
@@ -1,14 +1,26 @@
 interface CldImageProps {
-  id: string;
-  width: string;
-  ext: string;
+  dataAttributes: any; // data.attributes.image.data.attributes
+  width?: number;
+  alt?: string;
 }
 
-export default function CldImage({ id, width, ext }: CldImageProps) {
+export default function CldImage({
+  width,
+  alt,
+  dataAttributes,
+}: CldImageProps) {
+  const height = (width / dataAttributes.width) * dataAttributes.height;
+
   return (
     <div>
       <img
-        src={`https://res.cloudinary.com/dwrv366lk/image/upload/w_${width},f_auto/${id}${ext}`}
+        src={`https://res.cloudinary.com/dwrv366lk/image/upload/w_${width},f_auto/${dataAttributes.hash}${dataAttributes.ext}`}
+        width={width}
+        height={height}
+        decoding="async"
+        loading="lazy"
+        alt={alt || ""}
+        className="bg-slate-100"
       />
     </div>
   );
